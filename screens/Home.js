c27338c341cf46994e68b3d11bdcef84a7b058d6
Enzuo <potc.zone+git@gmail.com>
2019-03-02 15:28:43 +0100

    Add FlatList

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Button } from 'react-native';
 
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false,
+      items: [{ key : 'bob'}, 
+              {'key' : 'pomme'},
+              { key : 'caramel'}]
+    };
+  }
+
   launchScan = (event) => {
     const {navigate} = this.props.navigation;
 
@@ -15,7 +26,10 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Home screen</Text>
-
+        <FlatList
+          data={this.state.items}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
         <Button
           onPress={this.launchScan}
           title="Scan"
@@ -33,5 +47,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
