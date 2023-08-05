cb791dc58a37ebb83b52e260d6b4bbb63ee30572
Enzuo <potc.zon.e+git@gmail.com>
2023-08-05 16:47:21 +0200

    Update config

--- src/logic/storage.js
+++ src/logic/storage.js
@@ -2,7 +2,8 @@ import database from '../database/database'
 
 /**
  * Simple middleman between logic and database storage
- * @param {*} name
+ * @param {string} name
+ * @param {object=} payload
  */
 export async function call(name, payload) {
   let result = await database.queryFile(name, payload)
